0dfe70f6c5951e76cef3be0e314813b3760262e8
Carinou <carinamalleville@gmail.com>
2021-11-26 09:09:49 +0100

    ajout classe Message et Database manager

---
+++ src/main/java/DataBaseManager.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class DataBaseManager {
+
+	public User user1;
+	public User user2;
+	public ArrayList<Message> Liste_messages;
+}
---
+++ src/main/java/Message.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Message {
+	
+	public User emetteur;
+	public User recepteur;
+	public String contenu;
+	public Date date;
+
+}
