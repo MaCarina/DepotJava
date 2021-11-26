4d61c591d49711a76ebb466397b8d7a160ec8dc8
Carinou <carinamalleville@gmail.com>
2021-11-26 09:03:17 +0100

    création classe user

---
+++ src/main/java/User.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class User {
+	
+	public String login;
+	public String password;
+	public String pseudo;
+	public boolean etat;
+	public ArrayList<User> activeMembers;
+	
+	public User(String login,String password,String pseudo,boolean etat,ArrayList<User> activeMembers){
+		this.login = login;
+		this.password = password;
+		this.pseudo = pseudo;
+		this.etat = etat;
+		this.activeMembers = activeMembers;
+	}
+	
+	public void setPseudo(String pseudo){
+		this.pseudo = pseudo;
+	}
+	
+	public String getPseudo(){
+		return this.pseudo;
+	}
+	
+	public boolean getEtat(){
+		return this.etat;
+	}
+	
+	public boolean PseudoValide(){
+		//doit regarder dans la BDD tous les pseudos
+		return true;
+	}
+	
+	public void modifyPseudo(){
+		
+	}
+}
